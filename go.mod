ffd1bcb1b2b7b08acd09eeb0c64c2cc8bc1026c2
Andrey K <andreyk.dev.box@gmail.com>
2022-05-24 23:50:27 +0300

    The implementation of a string reverse was added.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AndreyK42/golang-united-school-homework-4.1
+
+go 1.17
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,18 @@
 package reverse_string
 
+import "strings"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	if len(input) == 0 {
+		return input
+	}
+
+	var sb strings.Builder
+	var runes = []rune(input)
+
+	for i := len(runes) - 1; i >= 0; i-- {
+		sb.WriteRune(runes[i])
+	}
+
+	return sb.String()
 }
